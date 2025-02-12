8906957edc9d7e5376f5b57a5f86e413c7fac3c7
Kidist935 <abate.kidist@gmail.com>
2025-02-11 20:49:42 -0600

    banner folder modified

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
-import Banner from '../../Banner/Banner'
-import RowList from '../../Components/Rows/RowList/RowList'
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
+import RowList from "../../Components/Rows/RowList/RowList";
 
 function Home() {
   return (
     <>
-      <Header/>
-      <Banner/>
-      <RowList/>
-      <Footer/>
-
-
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
